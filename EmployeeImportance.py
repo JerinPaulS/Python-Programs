101fac208eb0e04745adcfd647f2fbc6548ae2bf
JerinPaulS <jerinsprograms@gmail.com>
2021-07-29 20:09:45 +0530

    Leetcode - 690 Employee Importance

---
+++ EmployeeImportance.py
@@ -0,0 +1,71 @@
+'''
+690. Employee Importance
+You have a data structure of employee information, which includes the employee's unique id, their importance value, and their direct subordinates' id.
+
+You are given an array of employees employees where:
+
+employees[i].id is the ID of the ith employee.
+employees[i].importance is the importance value of the ith employee.
+employees[i].subordinates is a list of the IDs of the subordinates of the ith employee.
+Given an integer id that represents the ID of an employee, return the total importance value of this employee and all their subordinates.
+
+ 
+
+Example 1:
+
+
+Input: employees = [[1,5,[2,3]],[2,3,[]],[3,3,[]]], id = 1
+Output: 11
+Explanation: Employee 1 has importance value 5, and he has two direct subordinates: employee 2 and employee 3.
+They both have importance value 3.
+So the total importance value of employee 1 is 5 + 3 + 3 = 11.
+Example 2:
+
+
+Input: employees = [[1,2,[5]],[5,-3,[]]], id = 5
+Output: -3
+ 
+
+Constraints:
+
+1 <= employees.length <= 2000
+1 <= employees[i].id <= 2000
+All employees[i].id are unique.
+-100 <= employees[i].importance <= 100
+One employee has at most one direct leader and may have several subordinates.
+id is guaranteed to be a valid employee id.
+'''
+"""
+# Definition for Employee.
+class Employee(object):
+    def __init__(self, id, importance, subordinates):
+    	#################
+        :type id: int
+        :type importance: int
+        :type subordinates: List[int]
+        #################
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution(object):
+    def getImportance(self, employees, id):
+        """
+        :type employees: List[Employee]
+        :type id: int
+        :rtype: int
+        """
+		employee_table = {}
+        for i in range(len(employees)):
+            employee_table[employees[i].id] = i
+
+        stack = [id]
+        total_importance = 0
+        while stack:
+
+            curr = stack.pop()
+            total_importance += employees[employee_table[curr]].importance
+            for subs in employees[employee_table[curr]].subordinates:             
+                stack.append(subs)
+        return total_importance
