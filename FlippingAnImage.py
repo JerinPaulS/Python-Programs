d297e7da028659f972eb9df7dab1d61bb3974103
JerinPaulS <jerinsprograms@gmail.com>
2021-08-05 11:19:23 +0530

    Leetcode - 832 Flipping an Image

---
+++ FlippingAnImage.py
@@ -0,0 +1,62 @@
+'''
+832. Flipping an Image
+Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+
+To flip an image horizontally means that each row of the image is reversed.
+
+For example, flipping [1,1,0] horizontally results in [0,1,1].
+To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+
+For example, inverting [0,1,1] results in [1,0,0].
+ 
+
+Example 1:
+
+Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+Output: [[1,0,0],[0,1,0],[1,1,1]]
+Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+Example 2:
+
+Input: image = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ 
+
+Constraints:
+
+n == image.length
+n == image[i].length
+1 <= n <= 20
+images[i][j] is either 0 or 1.
+'''
+class Solution(object):
+    def flipAndInvertImage(self, image):
+        """
+        :type image: List[List[int]]
+        :rtype: List[List[int]]
+        """
+
+        def invert(bit):
+        	if bit == 1:
+        		return 0
+        	else:
+        		return 1
+
+        for row in range(len(image)):
+        	start = 0
+        	end = len(image[0]) - 1
+        	while start <= end:
+        		temp = image[row][start]
+        		image[row][start] = image[row][end]
+        		image[row][end] = temp
+        		if start == end:
+        			image[row][start] = invert(image[row][start])
+        		else:
+        			image[row][start] = invert(image[row][start])
+        			image[row][end] = invert(image[row][end])
+        		start = start + 1
+        		end = end - 1
+
+        return image
