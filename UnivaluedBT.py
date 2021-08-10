8d918ec912ec690ac3ed929b05da22d6a456cc27
JerinPaulS <jerinsprograms@gmail.com>
2021-08-10 15:36:29 +0530

    Leetcode - 965 Univalued Binary Tree

---
+++ UnivaluedBT.py
@@ -0,0 +1,50 @@
+'''
+965. Univalued Binary Tree
+A binary tree is uni-valued if every node in the tree has the same value.
+
+Given the root of a binary tree, return true if the given tree is uni-valued, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,1,1,1,1,null,1]
+Output: true
+Example 2:
+
+
+Input: root = [2,2,2,5,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 100].
+0 <= Node.val < 100
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isUnivalTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        
+        flag = [True]
+        def dfs(root, target):
+            if root is None:
+                return
+            dfs(root.left, target)
+            if root.val != target:
+                flag[0] = False
+            dfs(root.right, target)
+
+            return flag[0]
+
+        return dfs(root, root.val)
