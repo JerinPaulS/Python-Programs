f051d2b18efe7e479d1a2e67ebe300258827e3fc
JerinPaulS <jerinsprograms@gmail.com>
2021-08-14 12:40:26 +0530

    Leetcode - 1189 Maximum Number of Balloons

---
+++ MaxNumberofBalloons.py
@@ -0,0 +1,51 @@
+'''
+1189. Maximum Number of Balloons
+Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
+
+ 
+
+Example 1:
+
+
+
+Input: text = "nlaebolko"
+Output: 1
+Example 2:
+
+
+
+Input: text = "loonbalxballpoon"
+Output: 2
+Example 3:
+
+Input: text = "leetcode"
+Output: 0
+ 
+
+Constraints:
+
+1 <= text.length <= 104
+text consists of lower case English letters only.
+'''
+import collections
+class Solution(object):
+    def maxNumberOfBalloons(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+        string = "balloon"
+        char_freq = collections.Counter(text)
+        target_freq = collections.Counter(string)
+        count = len(text)
+        for char in string:
+        	count = min(count, char_freq[char] // target_freq[char])
+        return count
+
+obj = Solution()
+print(obj.maxNumberOfBalloons("krhizmmgmcrecekgyljqkldocicziihtgpqwbticmvuyznragqoyrukzopfmjhjjxemsxmrsxuqmnkrzhgvtgdgtykhcglurvppvcwhrhrjoislonvvglhdciilduvuiebmffaagxerjeewmtcwmhmtwlxtvlbocczlrppmpjbpnifqtlninyzjtmazxdbzwxthpvrfulvrspycqcghuopjirzoeuqhetnbrcdakilzmklxwudxxhwilasbjjhhfgghogqoofsufysmcqeilaivtmfziumjloewbkjvaahsaaggteppqyuoylgpbdwqubaalfwcqrjeycjbbpifjbpigjdnnswocusuprydgrtxuaojeriigwumlovafxnpibjopjfqzrwemoinmptxddgcszmfprdrichjeqcvikynzigleaajcysusqasqadjemgnyvmzmbcfrttrzonwafrnedglhpudovigwvpimttiketopkvqw"))
+#print(obj.maxNumberOfBalloons("loonbalxballpoon"))
+#print(obj.maxNumberOfBalloons("leetcode"))
+#print(obj.maxNumberOfBalloons("balon"))
