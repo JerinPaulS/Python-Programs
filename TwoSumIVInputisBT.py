746cfcf6303966b922dab647f60447af9d510790
JerinPaulS <jerinsprograms@gmail.com>
2021-08-23 12:54:14 +0530

    Leetcode Daily Challenge - Two Sum IV - Input is a BST

---
+++ TwoSumIVInputisBT.py
@@ -0,0 +1,66 @@
+'''
+Two Sum IV - Input is a BST
+Given the root of a Binary Search Tree and a target number k, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 9
+Output: true
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 28
+Output: false
+Example 3:
+
+Input: root = [2,1,3], k = 4
+Output: true
+Example 4:
+
+Input: root = [2,1,3], k = 1
+Output: false
+Example 5:
+
+Input: root = [2,1,3], k = 3
+Output: true
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-104 <= Node.val <= 104
+root is guaranteed to be a valid binary search tree.
+-105 <= k <= 105
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        visited_nodes = []
+
+        def search(root, k):
+        	if root is None:
+        		return False
+        	if (k - root.val) in visited_nodes:
+        		return True
+        	else:
+        		visited_nodes.append(root.val)
+
+        	leftsubtree = search(root.left, k)
+        	rightsubtree = search(root.right, k)
+
+        	return leftsubtree or rightsubtree
+
+        return search(root, k)
