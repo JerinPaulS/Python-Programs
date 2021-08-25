d5848295011631146cabf2b1fb0672d3b1ad8dd9
JerinPaulS <jerinsprograms@gmail.com>
2021-08-25 12:57:58 +0530

    Leetcode Study Plan - 876. Middle of the Linked List

---
+++ MiddleOfTheLLSP.py
@@ -0,0 +1,44 @@
+'''
+876. Middle of the Linked List
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+'''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        ptr1 = head
+        ptr2 = head
+        while ptr2 is not None and ptr2.next is not None:
+        	ptr1 = ptr1.next
+        	ptr2 = ptr2.next.next
+        return ptr1
