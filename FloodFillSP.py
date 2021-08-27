8fb1f76563e063de947e98394950a2d45238ad9e
JerinPaulS <jerinsprograms@gmail.com>
2021-08-27 15:29:24 +0530

    Leetcode Study Plan - 733. Flood Fill

---
+++ FloodFillSP.py
@@ -0,0 +1,60 @@
+'''
+733. Flood Fill
+An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+You are also given three integers sr, sc, and newColor. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with newColor.
+
+Return the modified image after performing the flood fill.
+
+ 
+
+Example 1:
+
+
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, newColor = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+Example 2:
+
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, newColor = 2
+Output: [[2,2,2],[2,2,2]]
+ 
+
+Constraints:
+
+m == image.length
+n == image[i].length
+1 <= m, n <= 50
+0 <= image[i][j], newColor < 216
+0 <= sr < m
+0 <= sc < n
+'''
+class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type newColor: int
+        :rtype: List[List[int]]
+        """
+        rows = len(image)
+        cols = len(image[0])
+        visited = []
+        oldColor = image[sr][sc]
+        que = []
+        que.append((sr, sc))
+        direction = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+        while len(que) > 0:
+        	x, y = que.pop()
+        	visited.append((x, y))
+        	image[x][y] = newColor
+        	for d in direction:
+        		if x + d[0] >= 0 and x + d[0] < rows and y + d[1] >= 0 and y + d[1] < cols:
+        			if image[x + d[0]][y + d[1]] == oldColor and (x + d[0], y + d[1]) not in visited:
+        				que.append((x + d[0], y + d[1]))
+        return image
