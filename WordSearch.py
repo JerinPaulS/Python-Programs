1af8539ac224122439176d2ea18e6cc52b3fde9c
JerinPaulS <jerinsprograms@gmail.com>
2021-09-12 14:39:58 +0530

    Leetcode - 79. Word Search

---
+++ WordSearch.py
@@ -0,0 +1,61 @@
+'''
+79. Word Search
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+ 
+
+Example 1:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+Example 2:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+Example 3:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+ 
+
+Constraints:
+
+m == board.length
+n = board[i].length
+1 <= m, n <= 6
+1 <= word.length <= 15
+board and word consists of only lowercase and uppercase English letters.
+ 
+
+Follow up: Could you use search pruning to make your solution faster with a larger board?
+'''
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        rows, cols = len(board), len(board[0])
+        visited = set()
+
+        def dfs(row, col, index):
+        	if index == len(word):
+        		return True
+        	if row < 0 or col < 0 or row >= rows or col >= cols or (row, col) in visited or board[row][col] != word[index]:
+        		return False
+        	visited.add((row, col))
+        	result = dfs(row + 1, col, index + 1) or dfs(row - 1, col, index + 1) or dfs(row, col + 1, index + 1) or dfs(row, col - 1, index + 1)
+        	visited.remove((row, col))
+        	return result
+
+        for row in range(rows):
+        	for col in range(cols):
+        		if dfs(row, col, 0):
+        			return True
+        return False
