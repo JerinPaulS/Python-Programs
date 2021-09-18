d3fc4e648d8dd15e19f661472be7df3b36713287
JerinPaulS <jerinsprograms@gmail.com>
2021-09-18 12:27:14 +0530

    Leetcode Study Plan - 139. Word Break

---
+++ WordBreak.py
@@ -0,0 +1,51 @@
+'''
+139. Word Break
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 300
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
+
+'''
+class Solution(object):
+	def wordBreak(self, s, wordDict):
+		"""
+		:type s: str
+		:type wordDict: List[str]
+		:rtype: bool
+		"""
+		result = [False] * (len(s) + 1)
+		result[-1] = True
+
+		for index in range(len(s) - 1, -1, -1):
+			for word in wordDict:
+				if index + len(word) <= len(s) and s[index:len(word)] == word:
+					result[index] = result[index + len(word)]
+				if result[index]:
+					break
+		return result[0]
