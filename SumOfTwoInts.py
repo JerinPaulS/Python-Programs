e725730bed64dc2140b393ef44bbc599a70b5464
JerinPaulS <jerinsprograms@gmail.com>
2021-09-19 13:18:26 +0530

    Leetcode - 371. Sum of Two Integers

---
+++ SumOfTwoInts.py
@@ -0,0 +1,36 @@
+'''
+371. Sum of Two Integers
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+ 
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = 2, b = 3
+Output: 5
+ 
+
+Constraints:
+
+-1000 <= a, b <= 1000
+'''
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        mask = 0xffffffff
+        while (b & mask) > 0:            
+            carry = ( a & b ) << 1
+            a = (a ^ b) 
+            b = carry
+        if b > 0:
+            return (a & mask)
+        else:
+            return a
