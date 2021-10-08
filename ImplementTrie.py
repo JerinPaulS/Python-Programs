660b868a471f27ccfe9c59680f90a0bd74cb25dc
JerinPaulS <jerinsprograms@gmail.com>
2021-10-08 13:05:42 +0530

    Leetcode Daily Challenge - 208. Implement Trie (Prefix Tree)

---
+++ ImplementTrie.py
@@ -0,0 +1,95 @@
+'''
+208. Implement Trie (Prefix Tree)
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+Implement the Trie class:
+
+Trie() Initializes the trie object.
+void insert(String word) Inserts the string word into the trie.
+boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+ 
+
+Example 1:
+
+Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+Output
+[null, null, true, false, true, null, true]
+
+Explanation
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True
+ 
+
+Constraints:
+
+1 <= word.length, prefix.length <= 2000
+word and prefix consist only of lowercase English letters.
+At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+'''
+class TrieNode(object):
+	def __init__(self):
+		self.children = {}
+		self.enfOfWord = False
+
+
+class Trie(object):
+    def __init__(self):
+    	self.root = TrieNode()
+        
+
+    def insert(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
+        curr = self.root
+
+        for char in word:
+        	if curr.children.has_key(char) == False:
+        		curr.children[char] = TrieNode()
+        	curr = curr.children[char]
+        curr.enfOfWord = True
+        
+
+    def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        curr = self.root
+
+        for char in word:
+        	if curr.children.has_key(char) == False:
+        		return False
+        	curr = curr.children[char]
+        return curr.enfOfWord
+        
+
+    def startsWith(self, prefix):
+        """
+        :type prefix: str
+        :rtype: bool
+        """
+        curr = self.root
+
+        for char in prefix:
+        	if curr.children.has_key(char) == False:
+        		return False
+        	curr = curr.children[char]
+        return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
