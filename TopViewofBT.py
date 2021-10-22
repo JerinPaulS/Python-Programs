7a649876403240a0b3c65eeb1fab8f68284e995b
JerinPaulS <jerinsprograms@gmail.com>
2021-10-22 13:12:02 +0530

    GFG Daily Challenge - Top View of Binary Tree

---
+++ TopViewofBT.py
@@ -0,0 +1,161 @@
+'''
+Top View of Binary Tree
+Given below is a binary tree. The task is to print the top view of binary tree. Top view of a binary tree is the set of nodes visible when the tree is viewed from the top. For the given below tree
+
+       1
+    /     \
+   2       3
+  /  \    /   \
+4    5  6   7
+
+Top view will be: 4 2 1 3 7
+Note: Return nodes from leftmost node to rightmost node.
+
+Example 1:
+
+Input:
+      1
+   /    \
+  2      3
+Output: 2 1 3
+Example 2:
+
+Input:
+       10
+    /      \
+  20        30
+ /   \    /    \
+40   60  90    100
+Output: 40 20 10 30 100
+Your Task:
+Since this is a function problem. You don't have to take input. Just complete the function topView() that takes root node as parameter and returns a list of nodes visible from the top view from left to right.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 ≤ N ≤ 105
+1 ≤ Node Data ≤ 105
+ 
+
+Note:The Input/Ouput format and Example given are used for system's internal purpose, and should be used by a user for Expected Output only. As it is a function problem, hence a user should not read any input from stdin/console. The task is to complete the function specified, and not to write the full code.
+'''
+#User function Template for python3
+
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+import collections
+class Solution:
+    
+    #Function to return a list of nodes visible from the top view 
+    #from left to right in Binary Tree.
+    def topView(self,root):
+    	if root is None:
+    		return []
+    	index_val = collections.defaultdict(list)
+    	result = []
+
+    	def dfs(node, height, level):
+    		if node is None:
+    			return
+    		if level not in index_val:
+    			index_val[level] = [node.data, height]
+    		elif index_val[level][1] > height:
+    			index_val[level] = [node.data, height]
+    		dfs(node.left, height + 1, level - 1)
+    		dfs(node.right, height + 1, level + 1)
+    		return
+    	dfs(root, 0, 0)
+    	sorted_dict = sorted(index_val.items(), key = lambda x: x[0])
+    	for pair in sorted_dict:
+    		result.append(pair[1])
+    	return result
+        
+        # code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        ob= Solution()
+        
+        res = ob.topView(root)
+        for i in res:
+            print(i,end=" ")
+        print()
+# } Driver Code Ends
