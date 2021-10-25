d10bea39f840a8b87860beee37806c604e2626c8
JerinPaulS <jerinsprograms@gmail.com>
2021-10-25 18:31:48 +0530

    Leetcode Study Plan - 376. Wiggle Subsequence

---
+++ MinSpaningTree.py
@@ -0,0 +1,88 @@
+'''
+Minimum Spanning Tree
+Given a weighted, undirected and connected graph of V vertices and E edges. The task is to find the sum of weights of the edges of the Minimum Spanning Tree.
+
+ 
+
+Example 1:
+
+Input:
+
+Output:
+4
+Explanation:
+
+The Spanning Tree resulting in a weight
+of 4 is shown above.
+Example 2:
+
+Input:
+
+Output:
+5
+Explanation:
+Only one Spanning Tree is possible
+which has a weight of 5.
+ 
+
+Your task:
+Since this is a functional problem you don't have to worry about input, you just have to complete the function  spanningTree() which takes number of vertices V and an adjacency matrix adj as input parameters and returns an integer denoting the sum of weights of the edges of the Minimum Spanning Tree. Here adj[i] contains a list of lists containing two integers where the first integer a[i][0] denotes that there is an edge between i and a[i][0] and second integer a[i][1] denotes that the distance between edge i and a[i][0] is a[i][1].
+
+Expected Time Complexity: O(ElogV).
+Expected Auxiliary Space: O(V2).
+ 
+
+Constraints:
+2 ≤ V ≤ 1000
+V-1 ≤ E ≤ (V*(V-1))/2
+1 ≤ w ≤ 1000
+Graph is connected and doesn't contain self loops & multiple edges.
+
+
+'''
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find sum of weights of edges of the Minimum Spanning Tree.
+    def spanningTree(self, V, adj):
+        #code here
+		selected_node = [0] * len(adj)
+		no_edge = 0
+		selected_node[0] = True
+		result = 0
+		while (no_edge < len(adj)):		    
+			minimum = float('inf')
+			for m in range(len(adj)):
+				if selected_node[m]:
+					for n in range(len(adj)):
+						if ((not selected_node[n]) and adj[m][n]):
+							if minimum > G[m][n]:
+								minimum = G[m][n]
+			result = result + minimum
+		print(result)
+
+'''			
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        V,E = map(int,input().strip().split())
+        adj = [[] for i in range(V)]
+        for i in range(E):
+            u,v,w = map(int,input().strip().split())
+            adj[u].append([v,w])
+            adj[v].append([u,w])
+        ob = Solution()
+        
+        print(ob.spanningTree(V,adj))
+# } Driver Code Ends
+'''
