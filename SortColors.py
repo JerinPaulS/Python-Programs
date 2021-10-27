f2565059125228e28ed79bb9654092f6b52bce0d
JerinPaulS <jerinsprograms@gmail.com>
2021-10-27 11:51:46 +0530

    Leetcode Study Plan - 75. Sort Colors

---
+++ SortColors.py
@@ -0,0 +1,54 @@
+'''
+75. Sort Colors
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+ 
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+Example 3:
+
+Input: nums = [0]
+Output: [0]
+Example 4:
+
+Input: nums = [1]
+Output: [1]
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+nums[i] is 0, 1, or 2.
+ 
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+'''
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        index, left, right = 0, 0, len(nums) - 1
+
+        while index <= right:
+        	if nums[index] == 0:
+        		nums[index], nums[left] = nums[left], nums[index]
+        		left = left + 1
+        	elif nums[index] == 2:
+        		nums[index], nums[right] = nums[right], nums[index]
+        		right = right - 1
+        		index = index - 1
+        	index = index + 1
