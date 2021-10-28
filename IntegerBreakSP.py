a68c49518edbbb4426cec22b464ce3a2cd1c6f0c
JerinPaulS <jerinsprograms@gmail.com>
2021-10-28 11:32:57 +0530

    Leetcode Study Plan - 343. Integer Break

---
+++ IntegerBreakSP.py
@@ -0,0 +1,37 @@
+'''
+343. Integer Break
+Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+
+Return the maximum product you can get.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: 2 = 1 + 1, 1 × 1 = 1.
+Example 2:
+
+Input: n = 10
+Output: 36
+Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+ 
+
+Constraints:
+
+2 <= n <= 58
+'''
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0] * (n + 1)
+        dp[1] = 1
+
+        for i in range(1, n + 1):
+        	for j in range(1, i):
+        		dp[i] = max(dp[i], dp[i - j] * j, (i - j) * j)
+        return dp[n]
