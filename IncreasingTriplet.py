fbd77489ee8d5350b1ba71aa3a676c707e7498da
JerinPaulS <jerinsprograms@gmail.com>
2022-01-14 14:44:08 +0530

    Leetcode Study Plan - 334. Increasing Triplet Subsequence

---
+++ IncreasingTriplet.py
@@ -0,0 +1,55 @@
+'''
+334. Increasing Triplet Subsequence
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+Example 2:
+
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+Example 3:
+
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 105
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?
+'''
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        min_sofar = []
+        max_sofar = []
+
+        for num in nums:
+        	if len(min_sofar) == 0:
+        		min_sofar.append(num)
+        	else:
+        		min_sofar.append(min(num, min_sofar[-1]))
+
+        for num in nums[::-1]:
+        	if len(max_sofar) == 0:
+        		max_sofar.append(num)
+        	else:
+        		max_sofar.append(max(num, max_sofar[-1]))
+
+        max_sofar = max_sofar[::-1]
+
+        for index, num in enumerate(nums):
+        	if num > min_sofar[index] and num < max_sofar[index]:
+        		return True
+
+        return False
