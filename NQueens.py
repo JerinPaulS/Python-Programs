7d27f1fb3c0fb11a54685e0f617e4f4f26759eb7
JerinPaulS <jerinsprograms@gmail.com>
2022-01-29 15:01:51 +0530

    Leetcode - 51. N-Queens

--- NQueens.py
+++ NQueens.py
@@ -1,4 +1,5 @@
 '''
+51. N-Queens
 The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
 
 Given an integer n, return all distinct solutions to the n-queens puzzle.
@@ -18,10 +19,42 @@ Example 2:
 Input: n = 1
 Output: [["Q"]]
 '''
-class Solution(object):
+class Solution:
     def solveNQueens(self, n):
-        """
-        :type n: int
-        :rtype: List[List[str]]
-        """
-        
+        result = []
+        board = []
+        for _ in range(n):
+            board.append(["."] * n)
+
+        cols = set()
+        posDiag = set()
+        negDiag = set()
+
+        def backTrack(row):
+            if row == n:
+                temp = []
+                for row in board:
+                    temp.append("".join(row))
+                result.append(temp)
+                return
+            for col in range(n):
+                if col in cols or (row - col) in negDiag or (row + col) in posDiag:
+                    continue
+
+                cols.add(col)
+                negDiag.add(row - col)
+                posDiag.add(row + col)
+                board[row][col] = "Q"
+
+                backTrack(row + 1)
+
+                cols.remove(col)
+                negDiag.remove(row - col)
+                posDiag.remove(row + col)
+                board[row][col] = "."
+
+        backTrack(0)
+        return result
+
+obj = Solution()
+print(obj.solveNQueens(4))        
